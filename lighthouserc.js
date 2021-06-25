8213697b0a86efabb2f74764ddd0cffa6a4788b3
Hiren <hhpatel6111@gmail.com>
2021-06-25 10:18:28 +0530

    continuous integration

---
+++ lighthouserc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    ci: {
+      collect: {
+        numberOfRuns: 3,
+        staticDistDir: "./dist/ng-github-lighthouse-ci",
+      },
+      assert: {
+        assertions: {
+          "first-contentful-paint": ["warn", { maxNumericValue: 4000 }],
+        },
+      },
+      upload: {
+        target: "temporary-public-storage",
+      },
+    },
+  };
+  
